2fb23453e61ce5606fbadf4d4c88e8cf1dc30e1b
Dev Nexus <osunkiyesitayo@gmail.com>
2022-08-19 23:25:11 +0100

    update again

--- src/components/HomeSections/Skills.jsx
+++ src/components/HomeSections/Skills.jsx
@@ -15,15 +15,60 @@ import classes from "./HomeSections.module.css";
 
 const Skills = () => {
   const skills = [
-    { icon: <FaHtml5 style={{ display: "inline" }} />, desc: "HTML 5" },
-    { icon: <FaCss3Alt style={{ display: "inline" }} />, desc: "CSS 3" },
-    { icon: <FaJsSquare style={{ display: "inline" }} />, desc: "JAVASCRIPT" },
     {
-      icon: <SiTypescript style={{ display: "inline" }} />,
+      icon: (
+        <FaHtml5
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
+      desc: "HTML 5",
+    },
+    {
+      icon: (
+        <FaCss3Alt
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
+      desc: "CSS 3",
+    },
+    {
+      icon: (
+        <FaJsSquare
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
+      desc: "JAVASCRIPT",
+    },
+    {
+      icon: (
+        <SiTypescript
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
       desc: "TYPESCRIPT",
     },
-    { icon: <FaReact style={{ display: "inline" }} />, desc: "REACT JS" },
-    { icon: <FaNode style={{ display: "inline" }} />, desc: "NODE JS" },
+    {
+      icon: (
+        <FaReact
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
+      desc: "REACT JS",
+    },
+    {
+      icon: (
+        <FaNode
+          className={`${classes["skill-icon"]}`}
+          style={{ display: "inline" }}
+        />
+      ),
+      desc: "NODE JS",
+    },
     {
       icon: <FaBootstrap style={{ display: "inline" }} />,
       desc: "BOOTSTRAP 5",
@@ -42,9 +87,7 @@ const Skills = () => {
         {skills.map((skill, i) => {
           return (
             <div key={i} className="col-6 col-sm-4 col-lg-3 mt-5">
-              <i
-                className={`${classes["skill-icon"]} fab fa-${skill.icon}`}
-              ></i>
+              {skill.icon}
               <p
                 style={{ fontSize: "0.8rem" }}
                 className="text-center mt-2 p-0"
